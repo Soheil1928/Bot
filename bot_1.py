97f7e9d9baa5b7a4eb1dc3c23074667921f07752
soheil <soheil.mahjoob73@gmail.com>
2024-03-02 13:58:13 +0330

    setup bot

---
+++ bot_1.py
@@ -0,0 +1,25 @@
+import telebot, requests, os
+
+TOKEN = os.getenv('TELEGRAM_TOKEN')
+bot = telebot.TeleBot(TOKEN)
+
+
+def send_data_to_server(data):
+    # url = 'https://django.darkube.app/information/'
+    url = 'http://django:8000/information/'
+    res = requests.post(url, data=data)
+    print(res.status_code)
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    data = {'first_name': message.from_user.first_name,
+            'last_name': message.from_user.last_name,
+            'telegram_username': message.from_user.username,
+            'telegram_id': message.chat.id}
+    bot.reply_to(message, text='Welcome')
+    send_data_to_server(data)
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
